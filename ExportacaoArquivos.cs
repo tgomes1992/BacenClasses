2147c4febedc3a34b8da3c00cc0f160e4e8b67f0
Thiago da Conceição Gomes <tt7.thiago@gmail.com>
2023-01-15 21:51:39 -0300

    Geração de Arquivos em .csv

---
+++ ExportacaoArquivos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BacenClasses
+{
+    public static class ExportacaoArquivos
+    {
+
+
+        public static void ExportacaoCsv(string path ,  IEnumerable<Registro> lista)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine("dataCotacao;fatorDiario;media");
+                foreach (Registro registro in lista)
+                {
+                    sw.WriteLine($"{ registro.dataCotacao }; { registro.fatorDiario} ; {registro.media}");
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
